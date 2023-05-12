6ca1abbad3db7e909125bc0b4786bb5eee184ef9
Jarod Ducamp <ducamp.jarod3011@gmail.com>
2023-05-12 12:02:44 +0200

    CRUD produit css

--- ShopMaquette/src/Form/ProduitType.php
+++ ShopMaquette/src/Form/ProduitType.php
@@ -3,21 +3,77 @@
 namespace App\Form;
 
 use App\Entity\Produit;
+use App\Entity\Fournisseur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ProduitType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('quantiter')
-            ->add('description')
-            ->add('prixPHUT')
-            ->add('note')
-            ->add('fournisseur')
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '50'
+                ],
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+                ])
+
+            ->add('quantiter', IntegerType::class, [
+                'label' => 'Quantité',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'min' => 1,              // la quantité minimum autorisée
+                    'max' => 100,           // la quantité maximum autorisée
+                ],
+                ])
+
+            ->add('description', TextareaType:: class,[
+                'attr' =>[
+                    'class' => 'form-control',
+                ],
+                'label' => 'Descritption',
+                'label_attr' =>[
+                    'class' => 'form-label mt-4'
+                ],
+                ])
+
+            ->add('prixPHUT', MoneyType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Prix PHUT',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                ])
+
+            ->add('fournisseur',EntityType::class,[
+                'attr' =>[
+                    'class' => 'form-control',
+                ],
+                'label' => 'Fournisseur',
+                'label_attr' =>[
+                    'class' => 'form-label mt-4'
+                ],
+                'class' => Fournisseur::class,
+            ])
+            
             ->add('souscategorie')
         ;
     }
