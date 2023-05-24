5ad88d099b5311a387841cc532707e76462f9bbe
Jarod Ducamp <ducamp.jarod3011@gmail.com>
2023-05-24 16:55:42 +0200

    fin front index et categorie debut pour souscategorie et produit

--- ShopMaquette/src/Form/RegistrationFormType.php
+++ ShopMaquette/src/Form/RegistrationFormType.php
@@ -4,20 +4,38 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType:: class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '180',
+                ],
+                'label' => 'Adresse email',
+                'label_attr' =>[
+                    'class' => 'form-label mt-2'
+                ],
+                'constraints' => [
+                    new Assert\Email(),
+                    new Assert\NotBlank(),
+                    new Assert\Length(['min' =>2, 'max' =>180])
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -30,10 +48,18 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '180'],
+                'label' => 'Password',
+                'label_attr' =>[
+                    'class' => 'form-label mt-2'
+                ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Entrer votre mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
